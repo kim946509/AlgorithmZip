11ef41453982e1956283e00574faf713671276cf
Your Name <kim946509@gmail.com>
2024-04-08 16:27:29 +0900

    전화번호리스트

--- src/programers/PhoneNumberList.java
+++ src/programers/PhoneNumberList.java
@@ -1,7 +1,7 @@
 package programers;
 
 import java.util.Arrays;
-import java.util.Comparator;
+import java.util.HashSet;
 
 public class PhoneNumberList {
     public static void main(String[] args) {
@@ -10,17 +10,15 @@ public class PhoneNumberList {
     }
 
     public static boolean solution(String[] phone_book) {
-        Arrays.sort(phone_book, Comparator.comparingInt(String::length));
 
-        for (int i = 0; i < phone_book.length-1; i++) {
-            for (int j = i+1; j < phone_book.length; j++) {
-                String sub = phone_book[j].substring(0,phone_book[i].length());
-                if(phone_book[i].equals(sub))
+        HashSet<String> phoneSet = new HashSet<>(Arrays.asList(phone_book));
+        for (int i = 0; i < phone_book.length; i++) {
+            for (int j = 0; j < phone_book[i].length(); j++) {
+                String substring = phone_book[i].substring(0, j);
+                if(phoneSet.contains(substring))
                     return false;
             }
-
         }
-
         return true;
     }
 }
