3be8629b70ac042ad5c2b550adf7aed92c568077
Your Name <kim946509@gmail.com>
2024-12-31 14:11:45 +0900

    2024/12/31 덱 배열을 통한 구현

--- src/barkingDog/test.java
+++ src/barkingDog/test.java
@@ -1,13 +1,11 @@
 package barkingDog;
 
+import barkingDog.deque.DequeImpl;
 import barkingDog.stack.Stack;
 
 public class test {
     public static void main(String[] args) {
-        Stack stack = new Stack();
-        stack.push(5);
-        stack.push(3);
-        stack.pop();
-        stack.top();
+        DequeImpl deque = new DequeImpl();
+        deque.test();
     }
 }
