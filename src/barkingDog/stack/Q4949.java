e29b0eaa061925beeb19de8d0544d1bc2fbc5324
Your Name <kim946509@gmail.com>
2025-01-02 15:43:51 +0900

    deque 구현체 성능을 테스트하기 위한 테스트 코드 작성

---
+++ src/barkingDog/stack/Q4949.java
@@ -0,0 +1,29 @@
+//package barkingDog.stack;
+//
+//import java.util.ArrayDeque;
+//import java.util.Deque;
+//import java.util.LinkedList;
+//import java.util.Scanner;
+//
+//public class Q4949 {
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        while(true){
+//            String sentence = sc.nextLine();
+//
+//            //종료 조건
+//            if(sentence.equals(".")){
+//                break;
+//            }
+//
+//            String result = "yes"; //결과 변수
+//            Deque<Character> stack = new LinkedList<>();
+//            for(int i=0;i<sentence.length();i++){
+//                char c = sentence.charAt(i);
+//                if(c=='.'){
+//                    break;
+//                }
+//            }
+//        }
+//    }
+//}
